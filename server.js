dcff8c0723df50e36a152faec87701de615a5135
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-04-03 16:43:45 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// const htmlRoutes = require("./routes/htmlRoutes");
+// const apiRoutes = require("./routes/apiRoutes");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded ({ extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(_dirname, "public")));
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, () => console.log('listening at http://localhost:${PORT}'));
