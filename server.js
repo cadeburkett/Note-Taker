a1f166c16a8682533014ee667944d1f88be43514
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-04-03 16:50:51 -0700

    added htmlRoutes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const htmlRoutes = require("./routes/htmlRoutes");
-// const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const path = require("path");
 const app = express();
 
