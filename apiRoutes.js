b0fa0dc3e19b7db55abb830a1c1d6ea9749c8dbf
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-04-12 01:46:17 -0700

    fixed some errors, still troubleshooting

--- apiRoutes.js
+++ apiRoutes.js
@@ -3,9 +3,10 @@ const path = require("path");
 const fs = require("fs");
 const uuidv4 = require("uuid");
 
+
 router.get("/api/notes", function(req,res) {
     fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err,data) {
-        console.log(data)
+        // console.log(data)
         if(err) throw err;
         res.json(JSON.parse(data));
     });
@@ -30,24 +31,24 @@ router.post("/api/notes", function(req, res) {
 
 });
 
-router.delete("/api/notes/:id", function(req, res) {
-    fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
-        if (err) throw err;
-        const savedMemo = JSON.parse(data);
-        const newArray = [];
-        for (let note of savedMemo) {
-            if(note.id === req.params.id) {;
-            newArray.push(note);
-        }
+// router.delete("/api/notes/:id", function(req, res) {
+//     fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
+//         if (err) throw err;
+//         const savedMemo = JSON.parse(data);
+//         const newArray = [];
+//         for (let note of savedMemo) {
+//             if(note.id === req.params.id) {
+//             newArray.push(note);
+//         }
 
-    };
+//     };
 
-    fs.writeFile(path.join__dirname, "db", "db.json"), JSON.stringify(newArray), function(err,data) {
-        if (err) throw err;
-        res.sendStatus(200);
+//     fs.writeFile(path.join__dirname, "db", "db.json"), JSON.stringify(newArray), function(err,data) {
+//         if (err) throw err;
+//         res.sendStatus(200);
 
-        };
-    });
-});
+//         };
+//     });
+// });
 
 module.exports = router
